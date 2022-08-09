80cb4835648055d5b45a7aa00b122d74d960d027
Vadym Kobzar <votvad@gmail.com>
2022-08-09 18:28:15 +0300

    again?

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,13 +4,25 @@ import { throttle } from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate',  throttle( e => {
-    localStorage.setItem('videoplayer-current-time', e.seconds);
-    }, 1000)
-    );
+const LOCALSTORAGE_KEY = localStorage.getItem("videoplayer-current-time");
+console.log(LOCALSTORAGE_KEY);
 
-player
-.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-.catch(function (error) {
-    console.error(error)
+
+player.setCurrentTime(LOCALSTORAGE_KEY).then(function (seconds) {
+    // seconds = реальное время попыток
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // время было меньше 0 или больше
+            break;
+
+        default:
+            // другая ошибка
+            break;
+    }
 });
+
+player.on('timeupdate', throttle(function (time) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(time.seconds));
+ 
+}, 1000));
