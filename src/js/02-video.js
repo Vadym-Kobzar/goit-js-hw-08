2c1f851562cbbf29824f92152249827fb8314ffc
Vadym Kobzar <votvad@gmail.com>
2022-08-09 18:18:25 +0300

    ops

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,11 +4,13 @@ import { throttle } from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+player.on('timeupdate',  throttle( e => {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+    }, 1000)
+    );
 
-player.on('timeupdate', throttle(onPlay, 1000));
-
-function onPlay({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', seconds);
-}
-
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player
+.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+.catch(function (error) {
+    console.error(error)
+});
