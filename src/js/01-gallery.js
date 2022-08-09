c54d5c7eeaf05a54e53f2a25a4725d38bb568dcf
Vadym Kobzar <votvad@gmail.com>
2022-08-09 21:42:24 +0300

    brain turned off

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,70 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+console.log(galleryItems);
+
 import SimpleLightbox from 'simplelightbox';
 
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const gallery = document.querySelector(".gallery");
-const itemsOfGallery = [];
+const gallery = document.querySelector('.gallery');
+const items = [];
 
-galleryItems.forEach((element) => {
-  /* <div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
-    />
-  </a>
-</div> */
-  const galleryItem = document.createElement("div");
-  galleryItem.className = "gallery__item";
-
-  const galleryLink = document.createElement("a");
-  galleryLink.className = "gallery__link";
+galleryItems.forEach(element => {
+  const galleryLink = document.createElement('a');
+  galleryLink.className = 'gallery__link';
   galleryLink.href = element.original;
-
-  const galleryImage = document.createElement("img");
-  galleryImage.className = "gallery__image";
+  const galleryImage = document.createElement('img');
+  galleryImage.className = 'gallery__image';
   galleryImage.src = element.preview;
-  galleryImage.setAttribute("data-source", element.original);
+  galleryImage.setAttribute('title', element.description);
   galleryImage.alt = element.description;
 
-  /*собрать гроздь и кинуть в массив */
-  galleryItem.append(galleryLink);
   galleryLink.append(galleryImage);
-  itemsOfGallery.push(galleryItem);
+  items.push(galleryLink);
 });
+gallery.append(...items);
 
-/*...spread call to mind*/
-gallery.append(...itemsOfGallery);
 new SimpleLightbox('.gallery a', {
-    captionDelay: 250,
-  });
-
-gallery.addEventListener("click", (event) => {
-  event.preventDefault();
-  if (event.target.nodeName !== "IMG") {
-    return;
-  }
-
-  const selectedImage = event.target.getAttribute("data-source");
-
-  const instance = basicLightbox.create(`
-    <img src="${selectedImage}" width="800" height="600">
-`);
-
-  instance.show();
-
-  gallery.addEventListener("keydown", (event) => {
-    if (event.key === "Escape") {
-      instance.close();
-    }
-    gallery.removeEventListener("keydown", event);
-  });
+  captionDelay: 250,
 });
-
-
-console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,10 @@
 import Player from '@vimeo/player';
-import { throttle } from 'lodash.throttle';
-
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
+import throttle from 'lodash.throttle';
 
 const CURRENT_TIME = "videoplayer-current-time";
 
-
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 player.on('timeupdate', throttle(onPlay, 1000));
    
 function onPlay ({ seconds }) {
